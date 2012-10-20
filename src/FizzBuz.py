1edc53593e9f1564e821bb61dc79ec885d7cc70d
edooby <rossi.he@gmail.com>
2012-10-20 19:35:42 +0200

    add seperate function

--- src/FizzBuz.py
+++ src/FizzBuz.py
@@ -5,21 +5,33 @@ Created on Oct 17, 2012
 '''
 
 class FizzBuzz(object):
+    
+    
+    def GetAnswerforBuzzRule(self): 
+        self.answer = 'Buzz'
+        return self.answer
+    
+    def GetAnswerforFizzRule(self): 
+        self.answer = 'Fizz'
+        return self.answer
+    
+    def GetAnswerforFizzBuzzRule(self): 
+        self.answer = 'FizzBuzz'
+        return self.answer
+    
     def getanswer(self,numbers):
-        
         list_to_return = []
-        
+          
         for number in numbers:
             answer = None
-            
             if number % 5 == 0:
-                answer = 'Buzz'
+                answer = self.GetAnswerforBuzzRule()
                 
             if number % 3 == 0:
-                answer = 'Fizz'
+                answer = self.GetAnswerforFizzRule()
             
             if number % 5 == 0 and number % 3 == 0:
-                answer = 'FizzBuzz'
+                answer = self.GetAnswerforFizzBuzzRule()
                 
             if answer == None:
                 answer = number              
